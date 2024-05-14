38baaecc24d7fc51363ec2585f432c4914f29efb
Sergio Andres Martinez E <sergio.martinez@fracttal.com>
2024-05-14 11:48:48 -0500

    add BlogFrom changes

--- components/BlogForm.tsx
+++ components/BlogForm.tsx
@@ -65,43 +65,43 @@ export default function BlogForm({ id, value }: BlogFromProps ) {
             <div>
                 <Label htmlFor="title">Titulo</Label>
                 <Input
-                type="text"
-                value={blogFrom.title}
-                onChange={(e) => {
-                    setBlogForm({ title: e.target.value });
-                }}
-                className="mt-2"
+                    type="text"
+                    value={blogFrom.title}
+                    onChange={(e) => {
+                        setBlogForm({ title: e.target.value });
+                    }}
+                    className="mt-2"
                 />
             </div>
             <div className="mt-5">
                 <Label htmlFor="description">Descripción</Label>
                 <Input
-                type="text"
-                value={blogFrom.description}
-                onChange={(e) => {
-                    setBlogForm({ description: e.target.value });
-                }}
-                className="mt-2"
+                    type="text"
+                    value={blogFrom.description}
+                    onChange={(e) => {
+                        setBlogForm({ description: e.target.value });
+                    }}
+                    className="mt-2"
                 />
             </div>
             <div className="mt-5">
                 <Label htmlFor="content">Content</Label>
                 <Editor
-                editorProps={{}}
-                defaultValue={blogFrom.content}
-                onDebouncedUpdate={updateContent}
-                className="border rounded pb-3 mt-2"
-                disableLocalStorage
+                    editorProps={{}}
+                    defaultValue={blogFrom.content}
+                    onDebouncedUpdate={updateContent}
+                    className="border rounded pb-3 mt-2"
+                    disableLocalStorage
                 />
             </div>
             <div className="flex flex-row-reverse gap-2 mt-4">
                 <Button className="mt-4 flex gap-2" onClick={onSubmit}>
-                <CheckIcon />
-                Guardar
+                    <CheckIcon />
+                    Guardar
                 </Button>
                 <Button className="mt-4 flex gap-2" variant={"secondary"}>
-                <Cross1Icon />
-                Cancelar
+                    <Cross1Icon />
+                    Cancelar
                 </Button>
             </div>
         </>
